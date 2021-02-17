c6802eca0d33840fc37f83a0627b2cadcab74eb3
tilenmiklavic <tm7321@student.uni-lj.si>
2021-02-17 11:32:39 +0100

    Portfolio page added

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -10,6 +10,31 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.addImages();
   }
 
+  numberOfImages: number = 27
+  firstColumn = []
+  secondColumn = []
+  thirdColumn = []
+
+  addImages(): void {
+    console.log("Adding images");
+    let columnNum = 0;
+
+    for (var i = 1; i <= this.numberOfImages; i++) {
+      let name = "../../assets/images/portfolio" + i + ".jpg"
+
+      if (columnNum == 0) {
+        this.firstColumn.push(name)
+        columnNum++
+      } else if (columnNum == 1) {
+        this.secondColumn.push(name)
+        columnNum++
+      } else {
+        this.thirdColumn.push(name) 
+        columnNum = 0
+      }
+    }
+  }
 }
