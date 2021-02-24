4fe522f8d3716efd3775b465a64856edd4ad8de0
tilenmiklavic <tm7321@student.uni-lj.si>
2021-02-24 12:01:47 +0100

    About page development

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,6 +12,48 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  skills = ["html", "css", "JavaScript", "Angular", "NodeJS", "Java", "Python"];
-
+  skills = [
+    {
+      "name": "html",
+      "icon": "fa-html5"
+    },
+    {
+      "name": "css",
+      "icon": "fa-css3-alt"
+    },
+    {
+      "name": "JavaScript",
+      "icon": "fa-js-square"
+    },
+    {
+      "name": "Angular",
+      "icon": "fa-angular"
+    },
+    {
+      "name": "NodeJS",
+      "icon": "fa-node-js"
+    },
+    {
+      "name": "Java",
+      "icon": "fa-java"
+    },
+    {
+      "name": "Python",
+      "icon": "fa-python"
+    }
+  ];
+  otherSkills = [
+    {
+      "name": "Photoshop",
+      "icon": "adobe-photoshop"
+    },
+    {
+      "name": "Lightroom",
+      "icon": "adobe-lightroom"
+    },
+    {
+      "name": "Illustrator",
+      "icon": "adobe-illustrator"
+    }
+  ];
 }
